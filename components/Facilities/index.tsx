b091931186d69e8f82f618dcb4c7fa2aa376702d
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-04-18 10:09:39 +0200

    made facilities state

--- components/Facilities/index.tsx
+++ components/Facilities/index.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Facilities.module.css";
 import { Col, Container, Row } from "react-bootstrap";
 import Facility from "./Facility";
 import ShowerOutlinedIcon from "@mui/icons-material/ShowerOutlined";
 
 const Facilities = () => {
+  const [facilities, setFacilities] = useState([
+    "bathroom",
+    "media & technology",
+    "safety & security",
+    "bedroom",
+    "food & drink",
+    "general",
+    "reception services",
+    "cleaning services",
+  ]);
+  
   const generateIcon = () => {
-    return <ShowerOutlinedIcon/>
-  }
+    return <ShowerOutlinedIcon />;
+  };
+
+  const generateFacilities = () => {
+    return facilities?.map((facility) => {
+      return (
+        <Col lg={4} md={4} sm={12} className='mb-4'>
+          <Facility icon={generateIcon} text={facility} />
+        </Col>
+      );
+    });
+  };
+
   return (
     <Container className={styles.container}>
       <div className={styles.title}>Facilities of This Hotel</div>
       <Row>
-        <Col lg={4} md={4} sm={12} className="mb-4">
+        {generateFacilities()}
+        {/* <Col lg={4} md={4} sm={12} className="mb-4">
           <Facility icon={generateIcon} text={'bathroom'} />
         </Col>
         <Col lg={4} md={4} sm={12} className="mb-4">
@@ -32,7 +55,7 @@ const Facilities = () => {
         </Col>
         <Col lg={4} md={4} sm={12} className="mb-4">
           <Facility icon={generateIcon} text={'bathroom'} />
-        </Col>
+        </Col> */}
       </Row>
     </Container>
   );
